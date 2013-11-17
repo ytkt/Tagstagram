088b87a6c45b451d0d4abe3e1d18f9bd1e58a757
Yuta Kato <yyttkktt@gmail.com>
2013-11-17 12:14:22 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tagstagram::Application.config.secret_key_base = '697178bb726076e890f5d09ad3fdb98ba1fb8a5df026b9bb9764efe8b1bb55dd8dc4ba69a2e2b2c3e3579b9959dfcbd23bd3083ab90a9a62f4262ac6cd3a70db'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tagstagram::Application.config.session_store :cookie_store, key: '_Tagstagram_session'
